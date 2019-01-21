2fb07a60f4ad22d353734f1e1db62ee433455de1
ilyassking <ilyas.ariba@gmail.com>
2019-01-21 18:40:51 +0100

    Add : Footer

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
           <h2 className='brand'>Github Trending</h2>
         </div>
         <List />
+        <footer className='footer'>
+          <p>This is my solution of the <a href='https://github.com/hiddenfounders/frontend-coding-challenge' target='_blank' rel='noopener noreferrer'>United remote web coding challenge</a>, Made with reactjs and github Api. </p>
+        </footer>
       </div>
     );
   }
